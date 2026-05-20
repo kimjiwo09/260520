5b0490a72dc34832975f51bc4f2aa62fddd040c5
kimjiwo09 <kjiwoo3571@gmail.com>
2026-05-20 13:58:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
+
 st.title('잠자고싶다 웹앱')
 st.write('안녕하세요!!😁')
+
+# 추가 기능 예시
+st.header('오늘의 수면 시간')
+sleep_time = st.slider('수면 시간을 선택하세요 (시간)', 0, 12, 8)
+st.write(f'목표 수면 시간: {sleep_time}시간 😴')
